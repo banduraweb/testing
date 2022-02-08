5016e3015dd7e2279adbf0a42bcd7bbfda3696e2
abandu <banduraweb@ukr.net>
2022-02-08 12:44:22 +0200

    login user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UserService } from './user.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @Serialize(UserDto)
   @Post('/signup')
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
+  }
+  @Post('/signin')
+  async login(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.login(createUserDto);
   }
 }
