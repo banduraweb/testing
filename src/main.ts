ab3f839c95a0f7a7833965b58cb0f66c7c81fd7a
abandu <banduraweb@ukr.net>
2022-02-16 15:27:39 +0200

    port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3030);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
