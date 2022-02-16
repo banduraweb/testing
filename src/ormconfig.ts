70cf74f98db2a2cd2125da4483c86283494c8e63
abandu <banduraweb@ukr.net>
2022-02-16 14:04:29 +0200

    pre-build

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,32 @@
+// import { ConnectionOptions } from 'typeorm';
+// require('dotenv').config();
+// const config: ConnectionOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: process.env.DBUSER,
+//   password: process.env.DBPASSWORD,
+//   database: 'testing',
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: false,
+//   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+//   cli: {
+//     migrationsDir: 'src/migrations',
+//   },
+// };
+//
+// export default config;
+
 import { ConnectionOptions } from 'typeorm';
 require('dotenv').config();
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: process.env.DBHOST,
+  port: +process.env.DBPORT,
   username: process.env.DBUSER,
   password: process.env.DBPASSWORD,
-  database: 'testing',
+  database: process.env.DB,
+  schema: process.env.SCHEMA,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
