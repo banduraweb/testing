dc12e96e9a27eab1860f457f6bed8dc12e2b7704
Anton Logvinenko <logvinenko.elit@gmail.com>
2022-10-09 11:52:40 +0300

    make it better

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,32 +1,12 @@
-// import { ConnectionOptions } from 'typeorm';
-// require('dotenv').config();
-// const config: ConnectionOptions = {
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: process.env.DBUSER,
-//   password: process.env.DBPASSWORD,
-//   database: 'testing',
-//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//   synchronize: false,
-//   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-//   cli: {
-//     migrationsDir: 'src/migrations',
-//   },
-// };
-//
-// export default config;
-
 import { ConnectionOptions } from 'typeorm';
 require('dotenv').config();
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'support',
-  // schema: process.env.SCHEMA,
+  host: process.env.DBHOST,
+  port: 32372,
+  username: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
@@ -36,3 +16,23 @@ const config: ConnectionOptions = {
 };
 
 export default config;
+
+// import { ConnectionOptions } from 'typeorm';
+// require('dotenv').config();
+// const config: ConnectionOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: '123456',
+//   database: 'support',
+//   // schema: process.env.SCHEMA,
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: false,
+//   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+//   cli: {
+//     migrationsDir: 'src/migrations',
+//   },
+// };
+
+// export default config;
